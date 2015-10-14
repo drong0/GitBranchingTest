72de655715f374118a26d69b85a101217e3a0002
Karl <karl.stiller@auteq.com.br>
2015-10-14 15:50:57 -0300

    Adding file to testing purposes
    Test file

---
+++ main.c
@@ -0,0 +1,20 @@
+/*!
+ * \file main.c
+ * \brief File just to test branching functions when in a team environment.
+ *
+ *
+ * \copyright	Copyright © 2015 by nobody in particular
+ */
+ 
+#include <stdlib.h>
+ 
+INT32S main( int argc, char *argv[] )
+{
+  //Eduardo create code in the space below this line.
+  //
+  //
+  //Wagner create code in the space below this line.
+  //
+  //
+  return EXIT_SUCCESS;
+}
